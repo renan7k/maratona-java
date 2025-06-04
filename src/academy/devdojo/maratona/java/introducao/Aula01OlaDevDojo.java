49607ae78128db7d65655a69ce2341d204d309ba
Renan Dias <renan7k@gmail.com>
2025-06-04 16:49:36 -0300

    Subindo projeto inicial

---
+++ src/academy/devdojo/maratona/java/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("Olá DevDojo");
+    }
+}
---
+++ src/academy/devdojo/maratona/java/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        //tipos que vão guardar em memória um valor simples
+        //int, double, float, char. byte, short, long, boolean
+        int idade = 40;
+        System.out.println("A idade é:"+idade);
+    }
+}
